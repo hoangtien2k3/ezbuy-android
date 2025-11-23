b343af8eaea3a1539ec46c596ec4c7b1b3187ec0
hoangtien2k3 <hoangtien2k3qx1@gmail.com>
2025-11-24 06:49:40 +0700

    Rename resources and update app name to Ezbuy; add new drawable and menu files

--- presentation/src/main/java/com/ezbuy/presentation/compose/ui/theme/Theme.kt
+++ presentation/src/main/java/com/ezbuy/presentation/compose/ui/theme/Theme.kt
@@ -39,7 +39,7 @@ private val LightColorScheme =
     )
 
 @Composable
-fun ProductAppTheme(
+fun EzbuyAppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
